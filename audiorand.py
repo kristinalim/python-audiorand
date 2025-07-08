15a40c5d24cb42fe92878593319add3ec2b5ea01
Kristina Lim <kristina.lim@bitcontroltech.com>
2025-07-08 12:19:18 +0800

    Launch menu from task bar

--- audiorand.py
+++ audiorand.py
@@ -7,7 +7,8 @@ from playsound import playsound
 from threading import Thread
 
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk, Gdk
+gi.require_version('AppIndicator3', '0.1')
+from gi.repository import Gtk, AppIndicator3
 
 AUDIO_STORAGE_FILE = os.path.expanduser("~/.audio_widget_files.txt")
 
@@ -22,39 +23,38 @@ def save_audio_files(files):
         for path in files:
             f.write(path + '\n')
 
-class AudioWidget(Gtk.Window):
+class AudioTrayApp:
     def __init__(self):
-        super().__init__(title="Audio Widget")
-        self.set_default_size(200, 100)
-        self.set_resizable(False)
-        self.set_border_width(10)
-
         self.audio_files = load_audio_files()
+        self.indicator = AppIndicator3.Indicator.new(
+            "audio-widget",
+            "media-playback-start",  # use an available icon or provide your own .svg/.png
+            AppIndicator3.IndicatorCategory.APPLICATION_STATUS
+        )
+        self.indicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
+        self.indicator.set_menu(self.build_menu())
 
-        self.button = Gtk.Button(label="Right-click me")
-        self.button.connect("button-press-event", self.on_button_press)
-        self.add(self.button)
+    def build_menu(self):
+        menu = Gtk.Menu()
 
-        self.show_all()
+        upload_item = Gtk.MenuItem(label="Upload Audio Files")
+        upload_item.connect("activate", self.on_upload_files)
+        menu.append(upload_item)
 
-    def on_button_press(self, widget, event):
-        if event.button == Gdk.BUTTON_SECONDARY:  # Right-click
-            menu = Gtk.Menu()
+        play_item = Gtk.MenuItem(label="Play Random Audio")
+        play_item.connect("activate", self.on_play_random_audio)
+        menu.append(play_item)
 
-            upload_item = Gtk.MenuItem(label="Upload Audio Files")
-            upload_item.connect("activate", self.on_upload_files)
-            menu.append(upload_item)
+        quit_item = Gtk.MenuItem(label="Quit")
+        quit_item.connect("activate", self.quit)
+        menu.append(quit_item)
 
-            play_item = Gtk.MenuItem(label="Play Random Audio")
-            play_item.connect("activate", self.on_play_random_audio)
-            menu.append(play_item)
+        menu.show_all()
+        return menu
 
-            menu.show_all()
-            menu.popup(None, None, None, None, event.button, event.time)
-
-    def on_upload_files(self, widget):
+    def on_upload_files(self, _):
         dialog = Gtk.FileChooserDialog(
-            title="Select Audio Files", parent=self,
+            title="Select Audio Files", parent=None,
             action=Gtk.FileChooserAction.OPEN
         )
         dialog.add_buttons(Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
@@ -75,7 +75,7 @@ class AudioWidget(Gtk.Window):
             save_audio_files(self.audio_files)
         dialog.destroy()
 
-    def on_play_random_audio(self, widget):
+    def on_play_random_audio(self, _):
         if not self.audio_files:
             self.show_message("No audio files available.")
             return
@@ -83,20 +83,21 @@ class AudioWidget(Gtk.Window):
         Thread(target=playsound, args=(audio_file,), daemon=True).start()
 
     def show_message(self, text):
-        md = Gtk.MessageDialog(
-            transient_for=self, flags=0,
+        dialog = Gtk.MessageDialog(
+            transient_for=None, flags=0,
             message_type=Gtk.MessageType.INFO,
             buttons=Gtk.ButtonsType.OK,
             text=text
         )
-        md.run()
-        md.destroy()
+        dialog.run()
+        dialog.destroy()
+
+    def quit(self, _):
+        Gtk.main_quit()
 
 def main():
-    win = AudioWidget()
-    win.connect("destroy", Gtk.main_quit)
+    AudioTrayApp()
     Gtk.main()
 
 if __name__ == "__main__":
     main()
-
